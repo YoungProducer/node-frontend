edbebb0de13718162af83b47f39a89f776140a01
YoungProducer <sashabezrukovownmail@gmail.com>
2019-11-28 00:08:01 +0200

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import {
+    createStore,
+    applyMiddleware,
+    Store,
+} from 'redux';
+import { Provider } from 'react-redux';
 
+import rootReducer from './store/reducers';
+import { loggerMiddleware } from './store/middleware/loggerMiddleware';
+import apiMiddleware from './store/middleware/apiMiddleware';
 import App from "./App";
 
-import "./index.css"
-
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 const client = new ApolloClient({
-  uri: 'http://localhost:4000'
+    uri: 'http://localhost:4000',
 });
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        loggerMiddleware,
+        apiMiddleware,
+    ),
+);
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  rootElement
+    <ApolloProvider client={client}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </ApolloProvider>,
+    rootElement,
 );
